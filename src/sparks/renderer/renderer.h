a39d30bfb17c81e56b2c711f042ccf5d153b7cd0
JasonLannel <jasonlanch@qq.com>
2023-12-31 08:39:21 +0800

    fix: Important error. Pull imme.

--- src/sparks/renderer/renderer.h
+++ src/sparks/renderer/renderer.h
@@ -70,7 +70,7 @@ class Renderer {
   void WorkerThread();
 
   RendererSettings renderer_settings_;
-  Scene scene_{"../../scenes/own_custom_scene.xml"};
+  Scene scene_{"../../scenes/base.xml"};
 
   /* CPU Renderer Assets */
   std::vector<glm::vec4> accumulation_color_;
